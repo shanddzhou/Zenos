e5da56e5397d2ebe3f5203ac642c4efb1f5e5c76
zhooo <shanddzhou@gmail.com>
2019-07-07 10:38:42 +0800

    first commit

---
+++ zenos-admin/src/main/java/com/zhoushan/blog/ZenosAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zhoushan.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenosAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenosAdminApplication.class, args);
+    }
+
+}
---
+++ zenos-core/src/main/java/com/zhoushan/blog/ZenosCoreApplication.java
@@ -0,0 +1,13 @@
+package com.zhoushan.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenosCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenosCoreApplication.class, args);
+    }
+
+}
